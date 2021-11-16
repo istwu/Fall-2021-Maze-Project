5c6e9445e2e61aae87b2f2686fb43aaf965e268f
itwu <itwu@email.wm.edu>
2021-11-15 22:56:51 -0500

    added barebones gui for AMazeActivity main menu

---
+++ app/src/main/java/edu/wm/cs/cs301/isabellawu/AMazeActivity.java
@@ -0,0 +1,21 @@
+package edu.wm.cs.cs301.isabellawu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view){
+        Intent intent = new Intent(this, GeneratingActivity.class);
+        startActivity(intent);
+    }
+}
