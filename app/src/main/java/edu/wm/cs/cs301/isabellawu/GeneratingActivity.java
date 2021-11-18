d8db39a865be80476ce8a60f785a06a1e61fce9a
itwu <itwu@email.wm.edu>
2021-11-18 15:03:35 -0500

    coded functionality for GeneratingActivity spinners

--- app/src/main/java/edu/wm/cs/cs301/isabellawu/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/isabellawu/GeneratingActivity.java
@@ -3,31 +3,139 @@ package edu.wm.cs.cs301.isabellawu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GeneratingActivity extends AppCompatActivity {
+
+    private int progress;
+    private int driver;
+    private boolean manual;
+    private int config;
+
+    private static final String TAG = "GeneratingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(100);
+        progress = progressBar.getProgress();
+
+        manual = false;
+        Spinner driverSpinner = (Spinner) findViewById(R.id.driverSpinner);
+        driverSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // 0 = none
+            // 1 = manual
+            // 2 = wizard
+            // 3 = wallfollower
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                driver = i;
+                String d = "";
+                switch(driver){
+                    case 0: {
+                        d = "None";
+                        manual = false;
+                        break;
+                    }
+                    case 1: {
+                        d = "Manual";
+                        manual = true;
+                        break;
+                    }
+                    case 2: {
+                        d = "Wizard";
+                        manual = false;
+                        break;
+                    }
+                    case 3: {
+                        d = "WallFollower";
+                        manual = false;
+                        break;
+                    }
+                }
+                Toast toast = Toast.makeText(getApplicationContext(), "Driver set to " + d, Toast.LENGTH_SHORT);
+                toast.show();
+                Log.v(TAG, "Driver set to " + d);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        Spinner configSpinner = (Spinner) findViewById(R.id.configSpinner);
+        configSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // 0 = none
+            // 1 = premium
+            // 2 = mediocre
+            // 3 = soso
+            // 4 = shaky
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                config = i;
+                String c = "";
+                switch(config){
+                    case 0: c = "None";
+                        break;
+                    case 1: c = "Premium";
+                        break;
+                    case 2: c = "Mediocre";
+                        break;
+                    case 3: c = "So-so";
+                        break;
+                    case 4: c = "Shaky";
+                }
+                Toast toast = Toast.makeText(getApplicationContext(), "Robot configuration set to " + c, Toast.LENGTH_SHORT);
+                toast.show();
+                Log.v(TAG, "Robot configuration set to " + c);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
     }
 
     public void startGame(View view) {
         // check if progress bar is at 100%
         // check if driver + config have been selected
-//        Log.v();
-
-        boolean test = true;
-        Intent intent;
-        if(!test) {
-            intent = new Intent(this, PlayManuallyActivity.class);
+        if(progress == 100 && driver != 0 && (manual || (!manual && config != 0))) {
+            boolean test = true;
+            Intent intent;
+            if(!test) {
+                intent = new Intent(this, PlayManuallyActivity.class);
+            }
+            else {
+                intent = new Intent(this, PlayAnimationActivity.class);
+            }
+            startActivity(intent);
         }
         else {
-            intent = new Intent(this, PlayAnimationActivity.class);
+            if(progress != 100) {
+                Log.v(TAG, "" + progress);
+                Toast toast = Toast.makeText(getApplicationContext(), "Maze not finished loading.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            if(driver == 0) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Need to choose a driver.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            if(!manual && config == 0) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Need to set robot configuration.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
-        startActivity(intent);
+
     }
 }
